ba4412a75a85e6bbfec85bb5e3570e84c055e547
Office Macbook <ppdb.annadzir@gmail.com>
2026-02-17 18:49:38 +0700

    feat: Add new Livewire component `StudentRecap` and its corresponding Blade view.

---
+++ app/Livewire/Admin/StudentRecap.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use Livewire\Component;
+
+class StudentRecap extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.student-recap');
+    }
+}
